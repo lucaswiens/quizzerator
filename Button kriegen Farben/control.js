bd51e34fd20d45497ab41714e34c644f161e98ef
Viktorrent <42324615+Viktorrent@users.noreply.github.com>
2018-10-20 21:06:31 +0200

    Add color highlight

---
+++ Button kriegen Farben/control.js
@@ -0,0 +1,45 @@
+/*holdingBox = document.getElementById("playerButtonHolder");
+holdingBox.children;
+var clrbtn = holdingBox.children;
+var color = [rgb(135, 0, 22), rgb(2, 112, 0 ), rgb(0, 62, 133), rgb(128, 130, 0), rgb(135, 88, 0), rgb(119, 0, 125)];
+
+		for(let i = 0; i < 7; i++) {
+
+      if(clrbtn == document.getElementById('button')
+      {
+				clrbtn[i].setAttribute('background-color', color[i]);
+			}
+		}
+
+
+    holdingBox = document.getElementById("playerButtonHolder:hover");
+    holdingBox.children;
+    var clrbtn = holdingBox.children;
+    var color = [rgb(255, 0, 42), rgb(22, 240, 97 ), rgb(0, 62, 133), rgb(8, 122, 252), rgb(241, 160, 9), rgb(226, 8, 237)];
+
+    		for(let i = 0; i < 7; i++) {
+
+          if(clrbtn == document.getElementById('button')
+          {
+    				clrbtn[i].setAttribute('background-color', color[i]);
+    			}
+    		}
+*/
+
+function highlight(group) {
+
+  var hcolor = ['rgb(255, 0, 0)', 'rgb(0, 255, 0 )', 'rgb(0, 0, 255)', 'rgb(125, 125, 0)', 'rgb(125, 0, 125)', 'rgb(0, 125, 125)'];
+
+  document.getElementById("gruppe"+group).setAttribute('style', 'background-color:' + hcolor[group]);
+}
+
+function high(element) {
+
+  var hcolor = ['rgb(255, 0, 0)', 'rgb(0, 255, 0 )', 'rgb(0, 0, 255)', 'rgb(125, 125, 0)', 'rgb(125, 0, 125)', 'rgb(0, 125, 125)'];
+
+  element.setAttribute('style', 'background-color:' + hcolor[group]);
+}
+
+function nohighlight(group) {
+ document.getElementById("gruppe"+group).setAttribute('style', '');
+}
